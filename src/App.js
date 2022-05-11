9e340ebf2099ea981b8dd3a537271cf055c1b805
Kevin Roney <ktroney.pdx@gmail.com>
2022-05-11 12:41:39 -0700

    set state for user in App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,8 @@ import { logout } from './services/fetch-utils';
 
 export default function App() {
   // You'll need to track the user in state
-
+  const [email, setEmail] = useState();
+  const [token, setToken] = useState();
   // add a useEffect to get the user and inject the user object into state on load
 
   async function handleLogout() {
